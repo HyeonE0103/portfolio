0f5c43cd3b09030aee4845ec4f107d613fabbb8a
HyeonE <mag30102000@gmail.com>
2023-12-07 15:04:34 +0900

    init: project initial setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Noto_Sans_KR } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const notoSansKr = Noto_Sans_KR({ subsets: ["latin"],
+weight: ["100", "500", "700", "900"]})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={notoSansKr.className}>{children}</body>
     </html>
   )
 }
