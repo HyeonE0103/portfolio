f1ab9f49b0ac685717b48bc92b90463b95260dac
HyeonE <mag30102000@gmail.com>
2023-12-20 23:26:56 +0900

    design: add tailwind css animations and fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,3 @@
-import type { Config } from 'tailwindcss';
-
 const plugin = require('tailwindcss/plugin');
 
 module.exports = {
@@ -10,14 +8,23 @@ module.exports = {
       lg: '1350px',
     },
     extend: {
+      keyframes: {
+        fadein: {
+          to: { transform: 'translateY(0)'},
+          from: { transform: 'translateY(40px)', opacity: '0' },
+        },
+      },
+      animation: {
+        fadein: 'fadein 2s ease-in-out',
+        fadein2: 'fadein 1s ease-in-out',
+      },
+      fontFamily: {
+        anton: ['Anton'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      fontFamily: {
-        lalezar: ['Lalezar'],
-        knewave: ['Knewave'],
       },
     },
   },
