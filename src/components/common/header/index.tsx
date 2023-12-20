e934123f46a2ec53ebeed5518227e24469913dfb
HyeonE <mag30102000@gmail.com>
2023-12-20 23:30:22 +0900

    design: add responsive ui to header

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -4,20 +4,21 @@ import React from 'react';
 export const Header = () => {
   const kind = ['INTRO', 'ABOUT', 'SKILL', 'PROJECT', 'CONTACT'];
   return (
-    <div className="z-50 fixed flex h-20 w-screen items-center justify-between px-7">
-      <Image
-        src={
-          'https://res.cloudinary.com/dozkdbzkh/image/upload/v1701953368/portfolio/gy734g0aukkhrxvy7cax.png'
-        }
-        alt="logo"
-        width={30}
-        height={30}
-      />
-      <ul className="flex gap-7 text-slate-50">
+    <div className="fixed z-50 flex h-14 w-screen items-center justify-between px-4 md:h-20 md:px-7">
+      <div className='relative w-6 h-6 md:w-7 md:h-7'>
+        <Image
+          src={
+            'https://res.cloudinary.com/dozkdbzkh/image/upload/v1701953368/portfolio/gy734g0aukkhrxvy7cax.png'
+          }
+          alt="logo"
+          fill
+        />
+      </div>
+      <ul className="flex gap-2 text-white md:gap-7">
         {kind.map((title, i) => (
           <li
             key={i}
-            className="pb-1 text-lg after:duration-250 relative cursor-pointer after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:bg-white after:transition-all hover:after:w-full"
+            className="after:duration-250 relative cursor-pointer pb-1 text-sm after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:bg-white after:transition-all hover:after:w-full md:text-lg"
           >
             {title}
           </li>
