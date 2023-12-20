8d34472db568605b0d3ec25568015ee7c7e31595
HyeonE <mag30102000@gmail.com>
2023-12-20 23:32:44 +0900

    data: organize data for skills section

---
+++ src/data/skills.ts
@@ -0,0 +1,96 @@
+export const LSKILLS = [
+  {
+    image:
+      'https://res.cloudinary.com/dozkdbzkh/image/upload/v1702544883/portfolio/skills/hskpsqjc4tyejvwhuj1t.png',
+    title: 'JavaScript',
+    content:
+      '언어를 이해하고 경험을 바탕으로 동적인 요소를 구현하고 개발할 수 있습니다.',
+  },
+  {
+    image:
+      'https://res.cloudinary.com/dozkdbzkh/image/upload/v1702544899/portfolio/skills/fwtb1unxxi6jnpoo9hho.png',
+    title: 'TypeScript',
+    content:
+      '언어를 이해하고 경험을 바탕으로 타입 시스템을 활용하여 안전성있는 개발을 할 수 있습니다.',
+  },
+  {
+    image:
+      'https://res.cloudinary.com/dozkdbzkh/image/upload/v1702544886/portfolio/skills/zu4r4y9bfo0ygser0uv0.png',
+    title: 'Html5',
+    content:
+      '웹접근성, 표준성을 준수하며 시멘틱태그를 이용해 마크업 할 수 있습니다.',
+  },
+];
+export const RSKILLS = [
+  {
+    image:
+      'https://res.cloudinary.com/dozkdbzkh/image/upload/v1702544884/portfolio/skills/zwmuvzentnrrzxawlvt3.png',
+    title: 'React',
+    content:
+      'React 구조와 여러가지 hook들을 잘 알고 있으며, 재사용가능한 컴포넌트 설계를 할 수 있습니다.',
+  },
+  {
+    image:
+      'https://res.cloudinary.com/dozkdbzkh/image/upload/v1702545925/portfolio/skills/qedfmpmz0eftlm1knvu7.svg',
+    title: 'Nextjs',
+    content:
+      '서버 사이드 렌더링을 구현하여 웹 어플리케이션을 구현할 수 있습니다',
+  },
+  {
+    image:
+      'https://res.cloudinary.com/dozkdbzkh/image/upload/v1702544890/portfolio/skills/sdno4pmy2tjyktzbp41v.png',
+    title: 'Redux',
+    content:
+      '액션, 리듀서, 스토어의 개념을 이해하고 활용하여 전역 상태 관리를 할 수 있습니다',
+  },
+  {
+    image:
+      'https://res.cloudinary.com/dozkdbzkh/image/upload/v1702544885/portfolio/skills/kesracobxm2jefbknfvn.png',
+    title: 'Recoil',
+    content:
+      'R아톰, 설렉터의 개념을 이해하고 활용하여 전역 상태 관리를 할 수 있습니다',
+  },
+  {
+    image:
+      'https://res.cloudinary.com/dozkdbzkh/image/upload/v1702544890/portfolio/skills/zsqlktdxgcwxb7pok0lm.svg',
+    title: 'React-Query',
+    content:
+      '데이터를 가져와 관리하며 캐싱과 로딩, 에러를 사용하여 효율적인 데이터 상태관리를 할 수 있습니다',
+  },
+  {
+    image:
+      'https://res.cloudinary.com/dozkdbzkh/image/upload/v1702545135/portfolio/skills/toclwzfhksaypjl3hucb.svg',
+    title: 'Swr',
+    content:
+      '데이터를 가져와 관리하며 캐싱과 로딩, 에러를 사용하여 효율적인 데이터 상태관리를 할 수 있습니다',
+  },
+];
+export const CSKILLS = [
+  {
+    image:
+      'https://res.cloudinary.com/dozkdbzkh/image/upload/v1702549718/portfolio/skills/zmofsf9jo8b01s5cmdip.png',
+    title: 'Styled-Components',
+    content:
+      '스타일을 적용하고 props를 활용하여 스타일을 동적으로 변경하며 반응형웹을 구현할 수 있습니다',
+  },
+  {
+    image:
+      'https://res.cloudinary.com/dozkdbzkh/image/upload/v1702545266/portfolio/skills/pzhcja2daokxbhbrm66e.png',
+    title: 'Emotion',
+    content:
+      '프로젝트에 스타일을 적용해본적이 있습니다',
+  },
+  {
+    image:
+      'https://res.cloudinary.com/dozkdbzkh/image/upload/v1702545961/portfolio/skills/p3sxkjpkgcuhojagiqn2.png',
+    title: 'TailWind',
+    content: '스타일을 적용하고 반응형 웹을 구현할 수 있습니다',
+  },
+  {
+    image:
+      'https://res.cloudinary.com/dozkdbzkh/image/upload/v1702544887/portfolio/skills/r6t7z1az75htalrpkcpg.png',
+    title: 'Css',
+    content:
+      '웹 디자인과 반응형웹을 적용할 수 있습니다.',
+  },
+];
