fef1c3204882514c23bcd89b6fdafb2f585aab13
HyeonE <mag30102000@gmail.com>
2023-12-20 23:31:31 +0900

    design: add responsive ui to intro page

--- src/components/pages/intro/index.tsx
+++ src/components/pages/intro/index.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 
 export const Intro = () => {
   return (
-    <div className="grid h-screen place-items-center bg-gradient-to-b from-black via-gray-950 to-stone-700 text-slate-50">
-      <div className="grid place-items-center gap-10">
-        <h1 className="text-7xl font-black drop-shadow-lg">
+    <div className="layout bg-gradient-to-b from-black via-gray-950 to-stone-700 text-white">
+      <div className="font-anton grid place-items-center">
+        <h1 className="animate-fadein2 text-5xl text-white opacity-90 md:text-8xl lg:text-9xl">
           FRONT-END DEVELOP
         </h1>
-        <h3 className="text-5xl font-bold drop-shadow-lg">PORTFOLIO</h3>
+        <h3 className="animate-fadein relative -top-4 text-4xl font-bold drop-shadow-lg md:text-7xl lg:text-8xl">
+          PORTFOLIO
+        </h3>
       </div>
       <UnderButton />
     </div>
