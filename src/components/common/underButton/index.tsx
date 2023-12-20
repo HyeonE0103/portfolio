bc0a15bbc38465c4d6ee99335f6d8a826338389c
HyeonE <mag30102000@gmail.com>
2023-12-20 23:30:42 +0900

    design: add responsive ui to underbutton

--- src/components/common/underButton/index.tsx
+++ src/components/common/underButton/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const UnderButton = () => {
   return (
-    <div className="fixed bottom-20 left-1/2">
-      <div className="animate-bounce grid h-12 w-12 cursor-pointer place-items-center rounded-full border-2 border-slate-50 text-slate-100 hover:drop-shadow-xl">
+    <div className="absolute bottom-10">
+      <div className="animate-bounce grid h-10 w-10 md:h-12 md:w-12 cursor-pointer place-items-center rounded-full border-2 border-slate-50 text-slate-100 hover:drop-shadow-xl">
         ▼
       </div>
     </div>
